ca376c4f8451e6dea2215a19dde23173aaa31de6
Razmik <Razo0088@mail.ru>
2025-11-17 00:57:57 +0500

    Update server.js

--- beck/server.js
+++ beck/server.js
@@ -30,7 +30,7 @@ const transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
         user: process.env.USER_EMAIL,  
-        pass: process.env.USER_EMAIL,
+        pass: process.env.USER_PASS,
     },
 });
 
